2490284dfc35e4129a25df0038bd7d5035251b68
Eric Robibaro <eric.robibaro@radialpoint.com>
2018-02-22 12:23:21 -0500

    Adding packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='snakeeater',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Eric Robibaro',
+    author_email='perlchild@perlchild.org',
+    description='Like kitchen, but with more python'
+)
